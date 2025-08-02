27479b73860830b377c6419ae747893712ce7cc1
l429609201 <429609201@qq.com>
2025-08-03 01:31:12 +0800

    modified:   src/scrapers/bilibili.py

--- src/scrapers/bilibili.py
+++ src/scrapers/bilibili.py
@@ -161,8 +161,8 @@ class BilibiliScraper(BaseScraper):
     _WBI_MIXIN_KEY_TABLE = [
         46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49,
         33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40,
-        61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62,
-        11, 36, 20, 34, 44, 16, 52
+        61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11,
+        36, 20, 34, 44, 52
     ]
 
     def __init__(self, pool: aiomysql.Pool):
@@ -283,7 +283,7 @@ class BilibiliScraper(BaseScraper):
             img_key = img_url.split('/')[-1].split('.')[0]
             sub_key = sub_url.split('/')[-1].split('.')[0]
             
-            mixin_key = "".join([(img_key + sub_key)[i] for i in self._WBI_MIXIN_KEY_TABLE])
+            mixin_key = "".join([(img_key + sub_key)[i] for i in self._WBI_MIXIN_KEY_TABLE])[:32]
             
             self._WBI_MIXIN_KEY_CACHE["key"] = mixin_key
             self._WBI_MIXIN_KEY_CACHE["timestamp"] = now
@@ -302,7 +302,7 @@ class BilibiliScraper(BaseScraper):
         sorted_params = sorted(params.items())
         
         # Create query string
-        query = urlencode(sorted_params)
+        query = urlencode(sorted_params, safe="!()*'")
         
         # Calculate signature
         signed_query = query + mixin_key
